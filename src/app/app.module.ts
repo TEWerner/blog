504acc1639aac82818500be2491714d7c136307b
Timur <timurwerner@web.de>
2023-06-13 15:11:47 +0200

    removed standard code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlogLayoutComponent } from './blog-layout/blog-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogLayoutComponent
   ],
   imports: [
     BrowserModule
